2b361bf519c77c3794429e2d7ae6cb6b4cef0ad4
Jeff Sutherland <jdsuther@asu.edu>
2017-04-02 10:56:05 +0700

    Update asset urls for aws s3

--- assets/js/make.js
+++ assets/js/make.js
@@ -15,7 +15,7 @@ function loadImages () {
   $.each(toppings, function (key, val) {
     var img = new Image();
     img.setAttribute('crossOrigin', 'anonymous');
-    img.src = '/assets/toppings/' + val.image;
+    img.src = '//s3-ap-southeast-1.amazonaws.com/pizza-luvrs-jsuth/toppings/' + val.image;
     val.img = img;
   });
 }
